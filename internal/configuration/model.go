25aab82c8f67ba417e378f98b7a1cd8b960e5ea2
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2023-08-10 13:49:23 -0600

    [FCR-04] added env vars config

---
+++ internal/configuration/model.go
@@ -0,0 +1,40 @@
+package configuration
+
+import "github.com/caarlos0/env"
+
+type ServerConfig struct {
+  Address AddressConfig
+  Scrapper ScrapperConfig
+  Database DatabaseConfig
+}
+
+type AddressConfig struct {
+  Port string `env:"HTTP_PORT" envDefault:":8081"`
+}
+
+type ScrapperConfig struct {
+  ExchangeRateUrl string `env:"EXCHANGE_RATE_URL"`
+  BasicPassiveRateUrl string `env:"TBP_URL"`
+}
+
+type DatabaseConfig struct {
+  SupabaseUrl string `env:"SUPABASE_URL"`
+  SupabaseKey string `env:"SUPABASE_KEY"`
+}
+
+func Read() (*ServerConfig, error) {
+  config := ServerConfig{}
+  if err := env.Parse(&config); err != nil {
+    return nil, err
+  }
+  if err := env.Parse(&config.Address); err != nil {
+    return nil, err
+  }
+  if err := env.Parse(&config.Scrapper); err != nil {
+    return nil, err
+  }
+  if err := env.Parse(&config.Database); err != nil {
+    return nil, err
+  }
+  return &config, nil
+}
