5547a6792c5c6233ee47252f77fe9be4bcf2b569
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2023-11-28 11:37:39 -0600

    [LIB-24] Fixed minimum date

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,7 +3,7 @@ package utils
 import "time"
 
 const DATE_FORMAT = "2006/01/02"
-const MINIMUM_YEAR = 1983
+const MINIMUM_YEAR = 1900
 const DEFAULT_DAYS_TO_GO_BACK = 30
 
 func GetDateFromDateToFromToday(days int) (time.Time, time.Time) {
